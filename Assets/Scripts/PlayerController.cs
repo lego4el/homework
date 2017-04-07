07b2ee4e5b90a549da0f8351f4ff57d1f6206a32
lego4el <kapusta958@gmail.com>
2017-04-07 17:53:41 +0300

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody _rb;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, vertical, 0f);
+        _rb.AddForce(direction * Speed);
+	}
+}
